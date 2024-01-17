2cccebb3080ec3932fad45c721d4fbfbfc13ee24
aleksandr.andreev <aleksandr.andreev@quadcode.com>
2024-01-17 16:23:54 +0200

    KS-1-3 task with first flying in space

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -5,12 +5,13 @@ fun main() {
     val year: Short = 1961
     var hour: String = "09"
     var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
     hour = "10"
     minute = "55"
 
-    println(year)
-//    println(hour)
-//    println(minute)
-    print(hour + ":")
-    print(minute)
+    println("$hour:$minute")
 }
