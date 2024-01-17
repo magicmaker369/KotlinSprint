bf9c052d9b13ff00e017bdc5636e41fbbfe8a2fc
aleksandr.andreev <aleksandr.andreev@quadcode.com>
2024-01-17 10:10:31 +0200

    KS-1-2 added new variable and output in console

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val quantityOfOrders: Byte = 75
+    val successMessageAfterPurchase: String = "Поздравляем! Вы совершили успешную покупку на нашем сайте!"
+    var quantityOfEmployees: Short = 2000
+    quantityOfEmployees = 1999
+
+    println(quantityOfOrders)
+    println(successMessageAfterPurchase)
+//    println(quantityOfEmployees)
+    println(quantityOfEmployees)
+}
