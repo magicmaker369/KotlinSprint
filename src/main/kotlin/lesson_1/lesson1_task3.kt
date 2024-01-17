5871e95473b71bd7bb8a8b980270f026dda181d7
aleksandr.andreev <aleksandr.andreev@quadcode.com>
2024-01-17 10:47:35 +0200

    KS-1-3 task with first flying in space

---
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_1
+
+fun main() {
+
+    val year: Short = 1961
+    var hour: String = "09"
+    var minute: String = "07"
+    hour = "10"
+    minute = "55"
+
+    println(year)
+//    println(hour)
+//    println(minute)
+    print(hour + ":")
+    print(minute)
+}
