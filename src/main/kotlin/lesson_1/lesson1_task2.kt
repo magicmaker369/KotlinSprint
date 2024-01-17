05da787023cc274c9af0b888377202dfe01a2427
aleksandr.andreev <aleksandr.andreev@quadcode.com>
2024-01-17 15:52:44 +0200

    KS-1-3 task with first flying in space

--- src/main/kotlin/lesson_1/lesson1_task2.kt
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -3,12 +3,12 @@ package org.example.lesson_1
 fun main() {
 
     val quantityOfOrders: Byte = 75
-    val successMessageAfterPurchase: String = "Поздравляем! Вы совершили успешную покупку на нашем сайте!"
+    val successMessage: String = "Поздравляем! Вы совершили успешную покупку на нашем сайте!"
     var quantityOfEmployees: Short = 2000
     quantityOfEmployees = 1999
 
     println(quantityOfOrders)
-    println(successMessageAfterPurchase)
+    println(successMessage)
 //    println(quantityOfEmployees)
     println(quantityOfEmployees)
 }
