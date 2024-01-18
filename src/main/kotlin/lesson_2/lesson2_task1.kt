880d830e244865d5090123f28d7841da3c0a937d
aleksandr.andreev <aleksandr.andreev@quadcode.com>
2024-01-18 21:00:13 +0200

    KS-2-1 task for find average score

--- src/main/kotlin/lesson_2/lesson2_task1.kt
+++ src/main/kotlin/lesson_2/lesson2_task1.kt
@@ -2,10 +2,23 @@ package org.example.lesson_2
 
 fun main() {
 
-    val quantityPeople = 4.0
-    val sumAllPoints = 3.0 + 4.0 + 3.0 + 5.0
+    val quantityPeople = 4
+    val pointFirstPeople = 3.0
+    val pointSecondPeople = 4.0
+    val pointThirdPeople = 3.0
+    val pointFourthPeople = 5.0
 
+    val sumAllPoints = pointFirstPeople + pointSecondPeople + pointThirdPeople + pointFourthPeople
     val averageScore = sumAllPoints / quantityPeople
 
     println(averageScore)
+
+    /**
+    решение с использованием extension sum()
+
+    val quantityPeople = 4
+    val sumAllPoints = listOf(3.0, 4.0, 3.0, 5.0)
+
+    println(sumAllPoints.sum() / 4)
+     */
 }
