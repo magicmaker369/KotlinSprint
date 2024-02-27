41e5e9f521308eb6f9692c34c6c07f2cba681302
aleksandr.andreev <aleksandr.andreev@quadcode.com>
2024-02-27 17:44:35 +0200

    KS-6-4 solution for task: guess the number in 5 attempts

--- src/main/kotlin/lesson_6/lesson6_task4.kt
+++ src/main/kotlin/lesson_6/lesson6_task4.kt
@@ -1,2 +1,37 @@
 package org.example.lesson_6
 
+import kotlin.random.Random
+
+/*
+Задача 4 к Уроку 6
+
+Напиши небольшую консольную игру, в которой нужно угадать число в промежутке от 1 до 9.
+Игроку дается 5 попыток, чтобы угадать это число.
+
+ – если игрок угадывает число, выводится сообщение “Это была великолепная игра!” и программа завершает работу;
+ – если не угадывает, то отображается "Неверно" или что-то на твое усмотрение и оставшееся количество попыток;
+ – после истечения попыток выводится сообщение “Было загадано число N”.
+ */
+
+fun main() {
+
+    val random = Random
+    val randomNumber = random.nextInt(1, 9)
+    var numberOfAttempts = 5
+
+    while (numberOfAttempts > 0) {
+        println("Введите число от 1 до 9:")
+        val enterDigit = readln().toInt()
+
+        if (randomNumber == enterDigit) {
+            println("Это была великолепная игра!")
+            return
+        } else if (randomNumber != enterDigit) {
+            println("Неверно, осталось попыток: ${--numberOfAttempts}")
+        }
+        if (numberOfAttempts == 0) {
+            println("Было загадано число $randomNumber")
+        }
+    }
+
+}
