6fca0990471881e377d90c1a21c7e5d358f9e562
aleksandr.andreev <magicmaker369@gmail.com>
2024-05-09 22:45:43 +0300

    KS-14-2 move Classes in Main.kt

--- src/main/kotlin/lesson_14_task2/Main.kt
+++ src/main/kotlin/lesson_14_task2/Main.kt
@@ -16,6 +16,76 @@ package org.example.lesson_14_task2
 Добавь метод вывода информации о свойствах корабля и выведи информацию в консоль.
  */
 
+open class LinerSecond(
+    val name: String,
+    val speed: Int,
+    val passengerCapacity: Int,
+) {
+
+    open fun printInfoShipSecond() {
+        println(
+            """
+            Тип корабля $name имеет следующие характеристики:
+            Скорость: $speed
+            Вместимость пассажиров: $passengerCapacity
+        """.trimIndent()
+        )
+    }
+
+    open fun printLoadingMethod() {
+        println("Способ погрузки у ${name}: Выдвигает горизонтальный трап со шкафута.")
+    }
+}
+
+class CargoSecond(
+    name: String,
+    speed: Int,
+    passengerCapacity: Int,
+    private val loadCapacity: Int,
+) : LinerSecond(name, speed, passengerCapacity) {
+
+    override fun printInfoShipSecond() {
+        println(
+            """
+            Тип корабля $name имеет следующие характеристики:
+            Скорость: $speed
+            Вместимость пассажиров: $passengerCapacity
+            Грузоподъемность: $loadCapacity
+        """.trimIndent()
+        )
+    }
+
+    override fun printLoadingMethod() {
+        println("Способ погрузки у ${name}: Активирует погрузочный кран.")
+    }
+}
+
+class IcebrakerSecond(
+    name: String,
+    speed: Int,
+    passengerCapacity: Int,
+    private val isSplittingIce: Boolean
+) : LinerSecond(name, speed, passengerCapacity) {
+
+    override fun printInfoShipSecond() {
+        println(
+            """
+            Тип корабля $name имеет следующие характеристики:
+            Скорость: $speed
+            Вместимость пассажиров: $passengerCapacity
+            ${
+                if (isSplittingIce) "Умеет колоть лёд: Да"
+                else "Умеет колоть лёд: Нет"
+            }
+        """.trimIndent()
+        )
+    }
+
+    override fun printLoadingMethod() {
+        println("Способ погрузки у ${name}: Открывает ворота со стороны кормы.")
+    }
+}
+
 fun main() {
 
     val linerSecond = LinerSecond("Лайнер", 100, 1300)
