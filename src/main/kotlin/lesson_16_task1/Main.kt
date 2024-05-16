08149bd2065114f27bf2d024039621419ce27258
aleksandr.andreev <magicmaker369@gmail.com>
2024-05-16 21:45:00 +0300

    KS-16-1 solution task: roll dice use class and private val

---
+++ src/main/kotlin/lesson_16_task1/Main.kt
@@ -0,0 +1,27 @@
+package org.example.lesson_16_task1
+
+import kotlin.random.Random
+
+/*
+Задача 1 к Уроку 16
+
+На игральном кубике может выпасть одно число от 1 до 6.
+
+Создай класс кубика, в котором будет проинициализировано одно поле со случайным числом.
+Единственный способ узнать это число – вызвать специальный метод, который напечатает информацию в консоль.
+ */
+
+class Dice() {
+
+    private val randomDigit = Random.nextInt(1, 6)
+
+    fun rollDice() {
+        println("Выпало число: $randomDigit")
+    }
+}
+
+fun main() {
+
+    val dice1 = Dice()
+    dice1.rollDice()
+}
